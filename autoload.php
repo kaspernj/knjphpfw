7bb4c85a3eaaa27a0665592ceee5e0686f8a398a
kaspernj <kaspernj@e82-103-131-27s.easyspeedy.dk>
2010-06-09 21:14:21 +0200

    ...

--- autoload.php
+++ autoload.php
@@ -7,16 +7,21 @@
 				"sqlite3" => "sqlite"
 			);
 			$this->knj = array(
-				"knj_web" => "web",
+				"web" => "web",
 				"knj_os" => "os",
 				"objects" => "objects",
 				"knjdb" => "db",
 				"knjobjects" => "objects"
 			);
+			$this->classes = array();
 		}
 		
 		function load($classname){
 			$class = strtolower($classname);
+			
+			if ($this->classes[$class]){
+				require_once($this->classes[$class]);
+			}
 			
 			if ($this->ext[$class]){
 				require_once("knj/exts.php");
@@ -25,6 +30,16 @@
 			
 			if ($this->knj[$class]){
 				require_once("knj/" . $this->knj[$class] . ".php");
+			}
+		}
+		
+		function add($class, $file = null){
+			if (is_array($class)){
+				foreach($class AS $key => $value){
+					$this->add($key, $value);
+				}
+			}else{
+				$this->classes[strtolower($class)] = $file;
 			}
 		}
 	}
