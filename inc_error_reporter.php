bf40198a9dfa1f7f643994e40939238817ffca55
Anders Jenbo <anders@jenbo.dk>
2012-03-11 14:07:06 +0100

    Allow error supression with @

--- inc_error_reporter.php
+++ inc_error_reporter.php
@@ -46,7 +46,7 @@ function knj_error_reporter_error_handeler($errno, $errmsg, $filename, $linenum,
         E_STRICT => 'Runtime Notice'
     );
 
-    if (!$args["hideerror"]) {
+    if (!$args["hideerror"] && error_reporting() !== 0) {
         echo $errortype[$errno] . ": " . utf8_encode($errmsg) . " in " . $filename . " on line " . $linenum . ".\n";
     }
 
@@ -244,8 +244,19 @@ function knj_error_shutdown()
     if (function_exists("error_get_last")) {
         $error = error_get_last();
 
-        if ($error["type"] == E_USER_ERROR || $error["type"] == E_CORE_ERROR || $error["type"] == E_COMPILE_ERROR || $error["type"] == E_ERROR) {
-            knj_error_reporter_error_handeler($error["type"], $error["message"], $error["file"], $error["line"], null, array("hideerror" => true));
+        if ($error["type"] == E_USER_ERROR
+			|| $error["type"] == E_CORE_ERROR
+			|| $error["type"] == E_COMPILE_ERROR
+			|| $error["type"] == E_ERROR
+		) {
+            knj_error_reporter_error_handeler(
+				$error["type"],
+				$error["message"],
+				$error["file"],
+				$error["line"],
+				null,
+				array("hideerror" => true)
+			);
         }
     }
 }
